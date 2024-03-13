ae2b5c21c5ce3870ccf6912f4e2df5cd72355ace
hometz <hometz2005@gmail.com>
2024-03-13 14:46:13 +0300

    4_lab_commit(1)

---
+++ MauiApp1/Services/RateService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Net.Http.Json;
+using System.Globalization;
+
+namespace MauiApp1.Services
+{
+    public class RateService : IRateService
+    {
+        public static HttpClient _client;
+
+        public RateService(HttpClient client)
+        {
+            _client = client;
+        }
+        public async Task<IEnumerable<Rate>> GetRates(DateTime date) 
+        {
+            string StringDate = date.ToString("yyyy-MM-dd");
+            var rates = await _client.GetFromJsonAsync<List<Rate>>
+                ($"https://api.nbrb.by/exrates/rates?ondate={StringDate}&periodicity=0");
+            return rates;
+        }
+    }
+}
